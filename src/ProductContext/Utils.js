d6d269267933022c775dc694c09ff9d4949c92ae
Tahir Ahmed <53032124+ttahm3d@users.noreply.github.com>
2022-03-14 20:14:12 +0530

    fix: sorting issue

--- src/ProductContext/Utils.js
+++ src/ProductContext/Utils.js
@@ -1,11 +1,12 @@
 const sortProducts = (state, products) => {
+  const sortedProducts = [...products];
   switch (state.sortBy) {
     case "low-to-high":
-      return products.sort(
+      return sortedProducts.sort(
         (prodA, prodB) => Number(prodA.price) - Number(prodB.price)
       );
     case "high-to-low":
-      return products.sort(
+      return sortedProducts.sort(
         (prodA, prodB) => Number(prodB.price) - Number(prodA.price)
       );
     default:
